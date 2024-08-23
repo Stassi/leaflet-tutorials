663c4087651b6fe173c586134cb15cbc47f25fcc
Andreas Stassivik <andreas@stassi.net>
2024-08-23 02:10:21 -0700

    zoom to detected location

--- public/script/mobile.js
+++ public/script/mobile.js
@@ -1,7 +1,11 @@
 import { addTileLayer, createWorldMap } from './mapUtils.js';
 import { attribution, urlTemplate } from './openStreetMap.js';
 
-const map = createWorldMap('map');
+const map = createWorldMap('map')
+  .locate({
+    maxZoom: 16,
+    setView: true
+  });
 
 addTileLayer({
   attribution,
