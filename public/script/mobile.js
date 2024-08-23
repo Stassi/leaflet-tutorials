e882c6a6619b93e45b5b26ec2e51e97a4a973967
Andreas Stassivik <andreas@stassi.net>
2024-08-23 15:21:12 -0700

    elements added on location found: - circle - marker

--- public/script/mobile.js
+++ public/script/mobile.js
@@ -1,10 +1,30 @@
-import { addTileLayer, createWorldMap } from './mapUtils.js';
+import {
+  addCircle,
+  addMarker,
+  addTileLayer,
+  createWorldMap
+} from './mapUtils.js';
 import { attribution, urlTemplate } from './openStreetMap.js';
 
 const map = createWorldMap('map')
   .locate({
     maxZoom: 16,
     setView: true
+  }).on('locationfound', ({
+    accuracy: radius,
+    latlng: latitudeLongitude
+  }) => {
+    addCircle({
+      latitudeLongitude,
+      map,
+      radius
+    })
+
+    addMarker({
+      latitudeLongitude,
+      map,
+      popupContent: `You are within ${radius} meters from this point.`
+    }).openPopup();
   });
 
 addTileLayer({
