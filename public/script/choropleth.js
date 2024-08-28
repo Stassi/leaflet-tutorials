8098260e198cfa2ad54d17e3e062ae9ab8b024d5
Andreas Stassivik <andreas@stassi.net>
2024-08-28 02:54:32 -0700

    formatting

--- public/script/choropleth.js
+++ public/script/choropleth.js
@@ -1,6 +1,11 @@
-import { addGeoJson, addTileLayer, createMap } from './map-utils.js';
+import {
+  addGeoJson,
+  addTileLayer,
+  createMap,
+} from './map-utils.js';
 import { attribution, urlTemplate } from './open-street-map.js';
-import data from '../data/us-state-population-density.json' with { type: 'json' };
+import data
+  from '../data/us-state-population-density.json' with { type: 'json' };
 
 const map = createMap({
   center: [37.8, -96],
