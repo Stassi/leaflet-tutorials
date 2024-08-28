cf6f2ba35eadd2cd60cf07f44cc3acc29566ee9c
Andreas Stassivik <andreas@stassi.net>
2024-08-28 03:00:26 -0700

    colorize US states based on population density

--- public/script/choropleth.js
+++ public/script/choropleth.js
@@ -23,4 +23,23 @@ addTileLayer({
 addGeoJson({
   data,
   map,
+  style({
+    properties: { density },
+  }) {
+    return {
+      color: 'white',
+      dashArray: '3',
+      fillColor: density > 1000 ? '#800026' :
+        density > 500 ? '#BD0026' :
+          density > 200 ? '#E31A1C' :
+            density > 100 ? '#FC4E2A' :
+              density > 50 ? '#FD8D3C' :
+                density > 20 ? '#FEB24C' :
+                  density > 10 ? '#FED976' :
+                    '#FFEDA0',
+      fillOpacity: 0.7,
+      opacity: 1,
+      weight: 2,
+    };
+  },
 });
