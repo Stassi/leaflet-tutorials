4e16baf051bed64edd95a9a24d3fc7f4535b2454
Andreas Stassivik <andreas@stassi.net>
2024-08-28 02:53:27 -0700

    render US states

---
+++ public/script/choropleth.js
@@ -0,0 +1,21 @@
+import { addGeoJson, addTileLayer, createMap } from './map-utils.js';
+import { attribution, urlTemplate } from './open-street-map.js';
+import data from '../data/us-state-population-density.json' with { type: 'json' };
+
+const map = createMap({
+  center: [37.8, -96],
+  id: 'map',
+  zoom: 4
+});
+
+addTileLayer({
+  attribution,
+  map,
+  urlTemplate,
+  zoomMax: 19,
+});
+
+addGeoJson({
+  data,
+  map,
+});
